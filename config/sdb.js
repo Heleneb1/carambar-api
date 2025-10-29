aad8f2204cd338453d1335539b0ea97a3c0ed820
HeleneB <helene.blanche@hotmail.fr>
2025-10-29 11:38:35 +0100

    Add initial jokes seed script and PostgreSQL config

--- config/sdb.js
+++ config/sdb.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require("sequelize");
-const path = require("path");
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: path.join(__dirname, "database.sqlite"),
-    logging: false,
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: { require: true, rejectUnauthorized: false },
+    },
 });
+
 module.exports = sequelize;
